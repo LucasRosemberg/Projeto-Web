1f574218d73597c9d25889e9124d2e5103aa31c8
LucasRosemberg <118315354+LucasRosemberg@users.noreply.github.com>
2022-11-22 21:07:33 -0300

    Inicio Projeto

---
+++ Aulas php/Projeto1/menu.php
@@ -0,0 +1,4 @@
+<div align=center>
+    <h2>Menu</h2>
+    <a href="?pg=conteudo">Home</a> | <a href="?pg=quemsomos">Quem somos</a> | <a href="?pg=contato">Fale conosco</a>
+</div>
---
+++ Aulas php/Projeto1/topo.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+
+    </head>
+    <body>
+        <div style="float: left">
+            <img src="Bolo.jpg" width=200px/>
+        </div>
+        <div style="background-color: brown">
+            <br>
+            <br>
+            <h1 style="color: white">Blog de Receitas</h1>
+            <br>
+        </div>
+        </div>
