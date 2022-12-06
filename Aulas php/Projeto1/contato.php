4ea2a4c32ecdad0cbfbc5181a6de3553423aa027
LucasRosemberg <118315354+LucasRosemberg@users.noreply.github.com>
2022-12-05 21:29:00 -0300

    Delete contato.php

--- Aulas php/Projeto1/contato.php
+++
@@ -1,7 +0,0 @@
-<h4>Fale conosco</h4>
-    <form action=?pg=sucesso.php>
-        <label> Nome: <input type="text" name="nome"/><br>
-        <label> E-mail: <input type="text" name="email"/><br>
-        <label> Cidade: <input type="text" name="cidade"/><br>
-        <label> Estado: <input type="text" name="uf"/><br>
-        <button>Enviar</button>
