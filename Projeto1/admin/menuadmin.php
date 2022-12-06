a22a283e75e10a35d0e957691856f214219673d6
LucasRosemberg <118315354+LucasRosemberg@users.noreply.github.com>
2022-12-06 08:44:45 -0300

    Update menuadmin.php

--- Projeto1/admin/menuadmin.php
+++ Projeto1/admin/menuadmin.php
@@ -2,5 +2,5 @@
   <a class="active" href="?pg=conteudoadmin">Início</a>
   <a href="?pg=dados">Dados</a>
   <a href="?pg=usuarios">Usuários</a>
-  <a href="http://localhost:81/Projeto1/">Logoff</a>
+  <a href="http://localhost/Projeto1/">Logoff</a>
 </div>
